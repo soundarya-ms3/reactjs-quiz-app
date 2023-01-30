430bafcc757ddc998bc73aaabe2779e0db686e07
soundarya-ms3 <soundaryams03@gmail.com>
2023-01-30 09:40:59 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./assets/style.css";
+
+class QuizBee extends Component{
+    render(){
+        return(
+            <div className="container">
+                <div className="title">QuizBee</div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<QuizBee />, document.getElementById("root"));
+
